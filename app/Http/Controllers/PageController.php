d461e328340c48d00cd97a55395f531e434e737a
stefano472 <stefanopiotti472@gmail.com>
2022-05-20 16:37:05 +0200

    es

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class PageController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        return view('welcome', compact('movies'));
+    }
+}
